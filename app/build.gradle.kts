a005b3788d2ad1a74307ff10d0552d1d676216b6
fjxw <harp.mtw@gmail.com>
2026-09-17 17:29:21 +0300

    Changes in Github Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       storeFile = file(keystorePath)
       storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
+      keyAlias = System.getenv("KEY_ALIAS")?.takeIf { it.isNotBlank() } ?: "my-key-alias"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
     create("debugConfig") {
